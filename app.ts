5f4745dbf8e11ce57cbdfc100953ae06675cc9fd
Nick Derevjanik <nick@networkbasedsolutions.com>
2023-09-16 16:34:28 -0700

    chore: simplify API file names

--- app.ts
+++ app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import treasureRouter from "./api/treasure/treasure-router";
+import treasureRouter from "./api/treasure/router";
 
 const app = express();
 app.use(express.json());
