f299dde73c23495573a538aeeb804848f4e42661
Nick Derevjanik <nick@networkbasedsolutions.com>
2023-09-19 00:33:59 -0700

    wip: validate response schema

--- api/treasures/router.spec.ts
+++ api/treasures/router.spec.ts
@@ -1,14 +1,25 @@
 import expect from "expect";
+import { resolve } from "node:path";
 import request from "supertest";
 import app from "../../app";
+import { generateSchema } from "../../test-utils";
+import { Treasure } from "./treasure";
 
 describe("GET /api/treasures/random", function () {
   it("should return random treasure data", async function () {
+    this.timeout(10000);
+
     const response = await request(app)
       .get("/api/treasures/random")
       .set("Accept", "application/json");
 
     expect(response.status).toBe(200);
-    // TODO: validate response schema matches Treasure schema
+
+    const schema = generateSchema(
+      resolve(__dirname, "./treasure.ts"),
+      Treasure.name
+    );
+
+    console.dir(schema, { depth: Infinity });
   });
 });
