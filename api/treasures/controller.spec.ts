4ea9d0ae7ac68c3a0c22041795391d2aa0f2b299
Nick Derevjanik <nick@networkbasedsolutions.com>
2023-09-23 18:34:54 -0700

    fix: unit test times out

--- api/treasures/controller.spec.ts
+++ api/treasures/controller.spec.ts
@@ -4,6 +4,7 @@ import { Treasure } from "./treasure";
 
 describe("getRandomTreasure", function () {
   it("should return a treasure object", async function () {
+    this.timeout(10000);
     expect(await getRandomTreasure()).toBeInstanceOf(Treasure);
   });
 });
