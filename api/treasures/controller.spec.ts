59e564397c0f6b75dd2c322194c80b8fee76ebdd
Nick Derevjanik <nick@networkbasedsolutions.com>
2023-09-26 15:35:01 -0700

    chore: use mocks when testing controllers

--- api/treasures/controller.spec.ts
+++ api/treasures/controller.spec.ts
@@ -1,12 +1,21 @@
-import expect from "expect";
+import { assert } from "chai";
+import { instance, mock, when } from "ts-mockito";
 import { TreasureController } from "./controller";
 import { Treasure } from "./treasure";
+import { TreasureGenerator } from "./treasure-generator";
 
 describe("getRandomTreasure", function () {
   it("should return a treasure object", async function () {
-    this.timeout(10000);
+    const treasure = new Treasure(
+      "unit test treasure",
+      "unit test description",
+      10
+    );
 
-    const controller = TreasureController.create();
-    expect(await controller.getRandomTreasure()).toBeInstanceOf(Treasure);
+    const mockTreasureGenerator = mock(TreasureGenerator);
+    when(mockTreasureGenerator.generateRandomTreasure()).thenResolve(treasure);
+
+    const controller = new TreasureController(instance(mockTreasureGenerator));
+    assert.deepEqual(await controller.getRandomTreasure(), treasure);
   });
 });
