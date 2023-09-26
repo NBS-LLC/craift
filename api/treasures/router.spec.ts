f346c8ab2b4a854482cef84dad6b15d9c5876339
Nick Derevjanik <nick@networkbasedsolutions.com>
2023-09-26 16:07:53 -0700

    chore: skip integration tests for CI
    Refs: #8

--- api/treasures/router.spec.ts
+++ api/treasures/router.spec.ts
@@ -8,6 +8,11 @@ import { Treasure } from "./treasure";
 
 describe("GET /api/treasures/random", function () {
   it("should return random treasure data", async function () {
+    if (process.env.CI === "true") {
+      // TODO: Configure CI for integration tests (#8)
+      this.skip();
+    }
+
     this.timeout(10000);
 
     const response = await request(app)
