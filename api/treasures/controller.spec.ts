093c8b38ba179a13ad4d3e8821eda28220ebef9f
Nick Derevjanik <nick@networkbasedsolutions.com>
2023-09-26 15:54:10 -0700

    chore: clean up unit test names

--- api/treasures/controller.spec.ts
+++ api/treasures/controller.spec.ts
@@ -4,18 +4,25 @@ import { TreasureController } from "./controller";
 import { Treasure } from "./treasure";
 import { TreasureGenerator } from "./treasure-generator";
 
-describe("getRandomTreasure", function () {
-  it("should return a treasure object", async function () {
-    const treasure = new Treasure(
-      "unit test treasure",
-      "unit test description",
-      10
-    );
+describe("TreasureController", function () {
+  describe("getRandomTreasure()", function () {
+    it("should return a treasure object", async function () {
+      const treasure = new Treasure(
+        "unit test treasure",
+        "unit test description",
+        10
+      );
 
-    const mockTreasureGenerator = mock(TreasureGenerator);
-    when(mockTreasureGenerator.generateRandomTreasure()).thenResolve(treasure);
+      const mockTreasureGenerator = mock(TreasureGenerator);
+      when(mockTreasureGenerator.generateRandomTreasure()).thenResolve(
+        treasure
+      );
 
-    const controller = new TreasureController(instance(mockTreasureGenerator));
-    assert.deepEqual(await controller.getRandomTreasure(), treasure);
+      const controller = new TreasureController(
+        instance(mockTreasureGenerator)
+      );
+
+      assert.deepEqual(await controller.getRandomTreasure(), treasure);
+    });
   });
 });
--- api/treasures/treasure.spec.ts
+++ api/treasures/treasure.spec.ts
@@ -2,14 +2,14 @@ import { Treasure } from "./treasure";
 import expect from "expect";
 
 describe("Treasure", () => {
-  describe("getAttributes", () => {
+  describe("getAttributes()", () => {
     it("should return an empty array on initialization", () => {
       const treasure = new Treasure("sword", "an old sword", 5);
       expect(treasure.attributes).toEqual([]);
     });
   });
 
-  describe("addAttribute", () => {
+  describe("addAttribute()", () => {
     it("should add an attribute to the treasure", () => {
       const treasure = new Treasure("sword", "an old sword", 5);
       treasure.addAttribute({ name: "damage", value: 1 });
